df1545355f8d14a57fcdc15904a8bcec1d671eb9
chemuel <chemuelgodes@gmail.com>
2022-09-08 08:03:39 +0800

    Activity 2 File

---
+++ printingMembers.java
@@ -0,0 +1,7 @@
+public class printname{
+    public static void main(String[] args) {
+      String firstMember = "Chemuel Castillo";
+      System.out.println(firstMember);
+      System.out.println("================");
+    }
+  }
