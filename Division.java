0d2497c2814fd0811b571584d0c673dc554a5382
johnkevs15 <john_kevin.campanilla@student.passerellesnumeriques.org>
2022-09-12 15:15:42 +0800

    kevin added

---
+++ Division.java
@@ -0,0 +1,11 @@
+public class Division {
+
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 2;
+        int c = a / b;
+        System.out.println("The Qoutient of a/b is" + c);
+
+    }
+
+}
