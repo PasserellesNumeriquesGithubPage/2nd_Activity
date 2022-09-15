8717fdbcda37618eedee4b73242b5b7a0159a59c
Jeane Cardiente <chemuelgodes@gmail.com>
2022-09-16 07:51:20 +0800

    Re edit Subtraction application

--- application.java
+++ application.java
@@ -1,11 +1,5 @@
-public class application{
-    public int Addition(int a, int b){
-        int a = 8;
-        int b = 3;
-        int c = a + b;
-        System.out.println("The Sum of a+b is" + c);
+public class Main {
+    int Subtraction(int a, int b) {
+        return a - b;
     }
 }
-
-
-
