8bc6d92a5f997565301717652f1aa41836bdc2a8
Jeane Cardiente <chemuelgodes@gmail.com>
2022-09-16 08:23:50 +0800

    Displayed

--- application.java
+++ application.java
@@ -1,11 +1,32 @@
-public class application{
-    public int Addition(int a, int b){
-        int a = 8;
-        int b = 3;
-        int c = a + b;
-        System.out.println("The Sum of a+b is" + c);
-    }
+import java.util.Scanner;
+
+public class Main{
+	public static void main(String[] args) {
+        Scanner inp= new Scanner(System.in);
+        int num1,num2;
+        System.out.println("Enter the first number: ");
+        num1 = inp.nextInt();
+        System.out.println("Enter second number: ");
+        num2 = inp.nextInt();
+        int ans;
+        System.out.println("Enter your selection: 1 for Addition, 2 for substraction 3 for Multiplication and 4 for division:");
+        int choose;
+        choose = inp.nextInt();
+        switch (choose){
+        case 1:
+            System.out.println("The result is: "+ add( num1,num2));
+            break;
+        case 2:
+            System.out.println("The result is: "+ sub( num1,num2));
+            break;      
+        case 3:
+            System.out.println("The result is: "+ mult( num1,num2));
+            break;
+        case 4:
+            System.out.println("The result is: "+ div( num1,num2));
+            break;
+            default:
+                System.out.println("Illigal Operation");
+        }
+	}
 }
-
-
-
